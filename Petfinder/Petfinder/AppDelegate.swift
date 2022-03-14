80737c46ef210ad3521871ca0ffe9227db097ac0
Dimitri Sopov <dimitri.sopov@gmail.com>
2022-03-14 17:47:36 +0100

    Double start flow fix

--- Petfinder/Petfinder/AppDelegate.swift
+++ Petfinder/Petfinder/AppDelegate.swift
@@ -13,11 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var coordinator: MainCoordinator?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        self.coordinator = MainCoordinator(window: window)
-        self.coordinator?.start()
-
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {        
         return true
     }
 
